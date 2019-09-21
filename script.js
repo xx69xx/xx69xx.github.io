cae0defbdc787b61effaab2d8106d6df427f48b6
xx69xx <55629127+xx69xx@users.noreply.github.com>
2019-09-21 17:22:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var ctx = canvas.getContext("2d");
 canvas.setAttribute("width", window.innerWidth-275);
 canvas.setAttribute("height", window.innerHeight-50);
 
-var player = {lvl:0, xp:0, dmg:10, hp:100};
-var slimes = {lvl:0, xp:0, hp:50, dmg:5};
+var player = {lvl:1, xp:0, dmg:10, hp:100, maxhp:100};
+var slimes = {lvl:1, xp:0, hp:50, dmg:5};
 var enemies = [{hp:slimes.hp, dmg:slimes.dmg}];
 
 var framecount = 0;
@@ -14,6 +14,9 @@ function frame(){
 
 	if(framecount%60==0){
 		attack();
+	}
+
+	if(framecount%120==0){
 		spawn();
 	}
 
@@ -24,38 +27,42 @@ function frame(){
 }
 
 function attack(){
+	for(var i = 0; i < enemies.length; i++){
+		player.hp -= enemies[i].dmg;
+		if(player.hp<=0){
+			alert("you died");
+			location.reload();
+		}
+	}
+	
 	enemies[0].hp -= player.dmg;
 	if(enemies[0].hp<=0){
 		enemies.shift();
-		player.xp += 25;
-		if(player.xp>=100){
-			player.xp = 0
+		player.xp += 100;
+		if(player.xp>=player.lvl*100){
+			player.xp = 0;
 			player.lvl++;
-			player.hp+=10;
+			player.maxhp+=10;
+			player.hp=player.maxhp;
 			player.dmg+=10;
 		}
 	}
 }
 
 function spawn(){
-	if(enemies.length == 0){
-		enemies.push({hp:slimes.hp, dmg:slimes.dmg});
-	}
+	enemies.push({hp:slimes.hp, dmg:slimes.dmg});
 }
 
 function menu(){
 	document.getElementById("player_lvl").innerHTML = player.lvl;
 	document.getElementById("player_xp").innerHTML = player.xp;
+	document.getElementById("player_maxxp").innerHTML = player.lvl*100;
 	document.getElementById("player_hp").innerHTML = player.hp;
+	document.getElementById("player_maxhp").innerHTML = player.maxhp;
 	document.getElementById("player_dmg").innerHTML = player.dmg;
 
 	document.getElementById("enemy_hp").innerHTML = enemies[0].hp;
 	document.getElementById("enemy_dmg").innerHTML = enemies[0].dmg;
-
-	document.getElementById("slimes_lvl").innerHTML = slimes.lvl;
-	document.getElementById("slimes_xp").innerHTML = slimes.xp;
-	document.getElementById("slimes_hp").innerHTML = slimes.hp;
-	document.getElementById("slimes_dmg").innerHTML = slimes.dmg;
 }
 
 function draw(){
@@ -74,11 +81,13 @@ function draw(){
 		ctx.fill();
 	}
 
-	ctx.fillStyle = "rgb("+Math.round(255-enemies[0].hp/50*255)+", "+Math.round(255-enemies[0].hp/50*255)+","+Math.round(255-enemies[0].hp/50*255)+")";
-	ctx.beginPath();
-	ctx.arc(canvas.width/4*3,canvas.height/2,25,0,Math.PI*2);
-	ctx.closePath();
-	ctx.fill();
+	for(var i = 0; i < enemies.length; i++){
+		ctx.fillStyle = "rgb("+Math.round(255-enemies[i].hp/50*255)+", "+Math.round(255-enemies[i].hp/50*255)+","+Math.round(255-enemies[i].hp/50*255)+")";
+		ctx.beginPath();
+		ctx.arc(canvas.width/4*2+i*60,canvas.height/2,25,0,Math.PI*2);
+		ctx.closePath();
+		ctx.fill();
+	}
 }
 
 frame();
